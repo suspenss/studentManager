5e66e69370956c51dba0e9181d2a35b84d96426c
epoche <tniasu@gmail.com>
2023-11-22 08:32:31 +0000

    将客户端模块化，添加注释

--- main.cpp
+++ main.cpp
@@ -1,11 +1,25 @@
 #include "manager.hpp"
 #include "server.hpp"
+#include <iostream>
 
 int main(int argc, char *argv[]) {
+    constexpr const char *open_message = R"(
+StudentManager with zzuli.
+  _________ __            .___             __       _____                                             
+ /   _____//  |_ __ __  __| _/____   _____/  |_    /     \ _____    ____ _____     ____   ___________ 
+ \_____  \\   __\  |  \/ __ |/ __ \ /    \   __\  /  \ /  \\__  \  /    \\__  \   / ___\_/ __ \_  __ \
+ /        \|  | |  |  / /_/ \  ___/|   |  \  |   /    Y    \/ __ \|   |  \/ __ \_/ /_/  >  ___/|  | \/
+/_______  /|__| |____/\____ |\___  >___|  /__|   \____|__  (____  /___|  (____  /\___  / \___  >__|   
+        \/                 \/    \/     \/               \/     \/     \/     \//_____/      \/       
+    )";
+
+    std::cout << open_message << std::endl;
+
     init_manager();
     start_server(argv[1], argv[2]);
 
     process_client();
+    exit_server();
 
     return 0;
 }
